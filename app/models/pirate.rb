21b00e9af37e21d73b2f815fadec096a1751294c
Bala Luguja <bluguja@gmail.com>
2020-10-22 21:28:23 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,9 @@
-class Pirate
+class Pirateclass Pirate
+    attr_accessor :name, :weight, :height 
+
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+    end
 end
